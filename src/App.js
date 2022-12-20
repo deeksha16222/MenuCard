0b022fe17c33afebbadefafb7dde753788f7cb0c
Deeksha1998 <deekshabisht16@gmail.com>
2022-12-20 17:57:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import Home from "./Home"
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-center mt-3"> All Items</h1>
+      <section className="py-4 container">
+        <div className="row justify-content-center"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
