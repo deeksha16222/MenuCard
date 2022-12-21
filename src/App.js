a19227f609cb137440c29f3422be19ddca3f8c84
Deeksha1998 <deekshabisht16@gmail.com>
2022-12-21 11:14:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
-import Home from "./Home"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FilterableProductTable from "./FilterableProductTable";
+
+const Products = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 
 function App() {
-  return (
-    <>
-    <Home/>
-    </>
-  );
+  return <FilterableProductTable products={Products} />;
 }
 
 export default App;
---
+++ src/FilterableProductTable.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
+export default FilterableProductTable;
